93a8b0734926420289a27c987745d504e33f91fa
Mat Marquis <mat@matmarquis.com>
2012-06-26 15:37:38 -0400

    Changed from min-width and max-width replicating data attributes to `media` attributes that just use matchMedia, reorg, and numerous whitespace changes — because who doesn’t love those?"

--- polyfill.js
+++ polyfill.js
@@ -1,5 +1,9 @@
 (function(win, undefined){
-	var detailsTest = (function() {
+	win.det = {
+		breakpointed : [],
+		closedicon : "<span class='icon i-triangle-right-blue'>+</span>",
+		openicon : "<span class='icon i-triangle-down-blue'>-</span>",
+		support : (function() {
 			var fakeBody,
 				doc = document,
 				de = doc.documentElement,
@@ -18,7 +22,7 @@
 			det.style.lineHeight = '7px';
 
 			div.appendChild(txt);
-			det.appendChild(div);	
+			det.appendChild(div);
 			bod.appendChild(det);
 
 			ret = div.offsetHeight === 0;
@@ -28,47 +32,46 @@
 
 			return ret;
 		})(),
-		siblings = function( oEl ) {
-			var sibs = [],
-				el = oEl.parentNode.firstChild;
+		collapseDetails : function() {
+			var windowWidth = window.innerWidth;
 
-			while( el = el.nextSibling ) {
-				if( el.nodeType === 1 && el !== oEl ) {
-					sibs.push( el );
-				}
-			}
-			return sibs;
-		},
-		collapseDetails = function() {
-			var windowWidth = window.innerWidth,
-				open = det.getAttribute("open") !== null;
+			for( var i = 0; i < det.breakpointed.length; i++ ) {
+				var bkpt = det.breakpointed[i],
+					open = win.matchMedia( bkpt.breakpoint ).matches;
 
-			for( var i = 0; i < breakpointed.length; i++ ) {
-				var point = breakpointed[i].breakpoint,
-					open = breakpointed[i].dir == "below" ? windowWidth >= point : windowWidth <= point;
+				bkpt.el[ ( open ? "set" : "remove" ) + "Attribute" ]( "open", "open" );
 
-				breakpointed[i].el[ ( open ? "set" : "remove" ) + "Attribute" ]( "open", "open" );
-
-				if( !detailsTest ) {
-					var summary = breakpointed[i].el.getElementsByTagName("summary")[0];
+				if( !det.support ) {
+					var summary = bkpt.el.getElementsByTagName("summary")[0];
 
 					polyfillToggle( summary, open );
 				}
 				// Update the aria attributes:
-				ariaStates( breakpointed[i].el, open );
+				win.det.ariaStates( bkpt.el, open );
 			}
 		},
-		polyfillToggle = function( el, open ) {
-			var content = siblings( el );
+		polyfillToggle : function( el, open ) {
+			var siblings = function( oEl ) {
+					var sibs = [],
+					el = oEl.parentNode.firstChild;
+
+				while( el = el.nextSibling ) {
+					if( el.nodeType === 1 && el !== oEl ) {
+						sibs.push( el );
+					}
+				}
+				return sibs;
+			},
+			content = siblings( el );
 
 			el.parentNode[ ( !open ? "set" : "remove" ) + "Attribute" ]( "open", "open" );
-			el.firstChild.innerHTML = !open ? leftarr : downarr;
+			el.firstChild.innerHTML = !open ? det.closedicon : det.openicon;
 
 			for( var i = 0; i < content.length; i++ ) {
 				content[i].style.display = ( open ? "block" : "none" );
 			}
 		},
-		ariaStates = function( el, open ) {
+		ariaStates : function( el, open ) {
 			// Get first-level contents of the `details` element:
 			var contents = el.children;
 
@@ -80,84 +83,82 @@
 				contents[j].nodeName !== "SUMMARY" && contents[j].setAttribute( "aria-hidden", !open );
 			}
 		},
-		details = document.getElementsByTagName("details"),
-		leftarr = "+",
-		downarr = "-",
-		breakpointed = [];
-	
-	// Loop through each of the `details` elements.
-	for( var i = 0; i < details.length; i++ ) {
-		var det = details[i],
-			summary = det.getElementsByTagName("summary")[0],
-			openBelow = det.getAttribute( "data-open-below" ),
-			openAbove = det.getAttribute( "data-open-above" ),
-			// If a value isn't set, on `data-open-below` and `data-open-above`, use five-hundo.
-			breakpoint = openBelow || openAbove || 500,
-			open = det.getAttribute("open") !== null;
+		init : function( ) {
+			var details = document.getElementsByTagName("details");
 
-		ariaStates( det, open );
+			// Loop through each of the `details` elements.
+			for( var i = 0; i < details.length; i++ ) {
+				var detail = details[i],
+					summary = detail.getElementsByTagName("summary")[0],
+					breakpoint = detail.getAttribute( "data-media" ),
+					open = detail.getAttribute("open") !== null;
 
-		// If there are breakpoints set, add the elements/details to a `breakpointed` array:
-		if( openBelow !== null || openAbove !== null ) {			
-			breakpointed.push( { "el" : det, dir : ( openBelow !== null ? "below" : "above" ), "breakpoint" : breakpoint } );
-		}
+				det.ariaStates( detail, open );
 
-			// Make sure we can navigate to the summary via the tab key:
-			summary.setAttribute( "tabindex", 0 );
-
-			if( !detailsTest ) {
-				// Create the little toggle arrow element:
-				var tog = document.createElement("div");
-			
-				tog.style.cssFloat = "left";
-				tog.style.paddingRight = ".5em";
-				tog.style.fontSize = ".9em";
-				tog.style.lineHeight = "1.2em";
-
-				tog.setAttribute("class", "details-toggle");
-
-				// Add the toggle to the start of the `summary`
-				summary.insertBefore( tog, summary.firstChild );
-			
-				// Set the initial indicator arrow dealie.
-				tog.innerHTML = !open ? downarr : leftarr;
-
-				polyfillToggle( summary, open );
-			}
-
-			summary.addEventListener("click", function(e) {
-				var det = this.parentNode,
-					open = det.getAttribute("open") !== null;
-
-				// Show/hide the `details` content in browsers that don’t support it natively.
-				if( !detailsTest ) {
-					polyfillToggle( this, open );
-					ariaStates( det, open );
-				} else {
-					// Update the aria attributes:
-					ariaStates( det, !open );
+				// If there are breakpoints set, add the elements/details to a `breakpointed` array:
+				if( breakpoint !== null ) {
+					det.breakpointed.push( { "el" : detail, "breakpoint" : breakpoint } );
 				}
-				// Now that the user is interacting with the page, stop triggering the breakpointed collapsibles on resize.
-				window.removeEventListener( "resize", collapseDetails );
-			});
 
-			if( !detailsTest ) {
-				// Trigger a click on the `summary` element when focused and enter or the spacebar are pressed:
-				summary.addEventListener("keydown", function(e) {
-					if( e.keyCode == 32 || e.keyCode == 13 ) {
-						var ev = document.createEvent("HTMLEvents");
+				// Make sure we can navigate to the summary via the tab key:
+				summary.setAttribute( "tabindex", 0 );
 
-						ev.initEvent("click", true, true);
-						this.dispatchEvent( ev, collapseDetails );	
+				if( !det.support ) {
+					// Create the little toggle arrow element:
+					var tog = document.createElement("div");
+
+					tog.style.cssFloat = "left";
+					tog.style.paddingRight = ".5em";
+					tog.style.fontSize = ".9em";
+					tog.style.lineHeight = "1.2em";
+
+					tog.setAttribute("class", "details-toggle");
+
+					// Add the toggle to the start of the `summary`
+					summary.insertBefore( tog, summary.firstChild );
+
+					// Set the initial indicator arrow dealie.
+					tog.innerHTML = !open ? det.openicon : det.closedicon;
+
+					det.polyfillToggle( summary, open );
+				}
+
+				summary.addEventListener("click", function(e) {
+					var detail = this.parentNode,
+						open = detail.getAttribute("open") !== null;
+
+					// Show/hide the `details` content in browsers that don’t support it natively.
+					if( !det.support ) {
+						det.polyfillToggle( this, open );
+						det.ariaStates( detail, open );
+					} else {
+						// Update the aria attributes:
+						det.ariaStates( detail, !open );
 					}
+					// Now that the user is interacting with the page, stop triggering the breakpointed collapsibles on resize.
+					window.removeEventListener( "resize", det.collapseDetails );
 				});
+
+				if( !det.support ) {
+					// Trigger a click on the `summary` element when focused and enter or the spacebar are pressed:
+					summary.addEventListener("keydown", function(e) {
+						if( e.keyCode == 32 || e.keyCode == 13 ) {
+							var ev = document.createEvent("HTMLEvents");
+
+							ev.initEvent("click", true, true);
+							this.dispatchEvent( ev, det.collapseDetails );
+						}
+					});
+				}
 			}
+			// If we have breakpointed `details` in that array, trigger them now and on window resize.
+			if( det.breakpointed.length ) {
+				det.collapseDetails();
+				window.addEventListener( "resize", det.collapseDetails );
+			}
+		}
 	}
 
-	// If we have breakpointed `details` in that array, trigger them now and on window resize.
-	if( breakpointed.length ) {
-		collapseDetails();
-		window.addEventListener( "resize", collapseDetails );
-	}
+	det.init();
 
 })(this);
